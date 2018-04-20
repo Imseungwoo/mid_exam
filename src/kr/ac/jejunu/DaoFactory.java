2ad3703412227eb9e9a4045818af3adc3aa56945
Imseungwoo <133332@naver.com>
2018-04-20 16:25:22 +0900

    exam5

--- src/kr/ac/jejunu/DaoFactory.java
+++ src/kr/ac/jejunu/DaoFactory.java
@@ -1,10 +1,15 @@
 package kr.ac.jejunu;
 
+import org.springframwork.context.annotation.Bean;
+import org.springframwork.context.annotation.Configuration;
+
 public class DaoFactory{
+    @Bean
     public ProductDao getProductDao() {
         return new ProductDao(getConnectionMaker());
     }
 
+    @Bean
     public ConnectionMaker getConnectionMaker() {
         return new JejuConnectionMaker();
     }
--- src/test/java/kr/ac/jejunu/ProductDaoTest.java
+++ src/test/java/kr/ac/jejunu/ProductDaoTest.java
@@ -1,18 +1,18 @@
 package kr.ac.jejunu;
 
 import org.junit.Test;
-
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import java.sql.SQLException;
 import org.junit.Before;
 
 public class ProductDaoTest {
     private ProductDao productDao;
     private DaoFactory daoFactory;
-//    private HallaProductDao hallaProductDao;
 
     public void setup() {
-        daoFactory = new DaoFactory();
-        productDao = daoFactory.getProductDao();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContest(DaoFactory.class);
+        productDao = applicationContext.getBean("productDao",ProductDao.class)
     }
 
     @Test
@@ -27,26 +27,4 @@ public class ProductDaoTest {
         assertEquals(title, product.getTitle());
         assertEquals(price, product.getPrice());
     }
-
-//    @Test
-//    public void hallaGet() throws SQLException, ClassNotFoundException {
-//        Long id = 1L;
-//        Product product = hallaProductDao.hallaGet(id);
-//        assertThat(product.getId(), is(1));
-//        assertThat(product.getTitle(), is("제주감귤"));
-//        assertThat(product.getPrice(), is(15000));
-//    }
-
-//    @Test
-//    public void hallaAdd() throws  SQLException, ClassNotFoundException {
-//        Product product = new Product();
-//        product.setTitle("한라봉");
-//        product.setPrice(30000);
-//        Long id = hallaProductDao.insert(product);
-//
-//        Product insertedProduct = hallaUserDao.get(id);
-//        assertThat(insertedProduct.getId(), is(id));
-//        assertThat(insertedProduct.getTitle(), is(product.getTitle()));
-//        assertThat(insertedProduct.getPrice(), is(product.getPrice));
-//    }
 }
