99db273ada7057f915515f803e1278c59d64714e
Imseungwoo <133332@naver.com>
2018-04-20 16:49:29 +0900

    Revert "exam6"
    This reverts commit 29638f3

--- src/kr/ac/jejunu/JejuConnectionMaker.java
+++ src/kr/ac/jejunu/JejuConnectionMaker.java
@@ -1,21 +1,11 @@
 package kr.ac.jejunu;
 
-import org.springframework.beans.factory.annotation.Value;
 import java.sql.*;
 
 public class JejuConnectionMaker implements ConnectionMaker{
-    @Value("${db.cassname}")
-    private String className;
-    @Value("${db.url}")
-    private String url;
-    @Value("${db.username}")
-    private String username;
-    @Value("${db.password}")
-    private String password;
-
     @Override
     public Connection getConnection() throws ClassNotFoundException, SQLException{
-        Class.forName(className);
-        return DriverManeger.getConnection(url, username, password);
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManeger.getConnection("jdbc://mysql://localhost/portal?characterEncoding=utf8", "root", "1111");
     }
 }
